97a4b2653fe660d0a1dabba06d4ea357b81f4181
Jurijs Kolesnikovs <jurijs_kolesnikovs@inbox.lv>
2018-12-20 12:08:59 +0200

    Installe gulp, working fine

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp'),
+    rigger = require('gulp-rigger'),
+    uglify = require('gulp-uglify'),
+    browserify = require('gulp-browserify'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cleanCSS = require('gulp-clean-css'),
+    clean = require('gulp-clean'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync'),
+    reload = browserSync.reload;
+
+var config = {
+    server: {
+        baseDir: './'
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 9000,
+    logPrefix: "Server"
+};
+
+gulp.task('webserver', function () {
+    browserSync(config);
+});
+
+gulp.task('js:build', function() {
+    return gulp.src(['./app.js'])
+        .pipe(browserify({
+            insertGlobals : true,
+            debug : !gulp.env.production
+        }))
+        //.pipe(uglify()) /* Remove comment for build */
+        .pipe(gulp.dest('build/'))
+        .pipe(reload({stream: true}));
+})
+
+gulp.task('sass:build', function() {
+    return gulp.src('assets/sass/main.sass')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('build/'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('clean', function() {
+    return gulp.src('build/*', { read: false })
+               .pipe(clean());
+});
+
+gulp.task('watch', function() {
+    watch(['app.js', 'model/*.js', 'router/*.js', 'controller/*.js'], function(event, cb) {
+        gulp.start('js:build');
+    });
+        
+    watch(['assets/sass/**/*.sass'], function(event, cb) {
+        gulp.start('sass:build');
+    });
+});
+
+gulp.task('default', ['webserver', 'js:build', 'sass:build', 'watch'])
