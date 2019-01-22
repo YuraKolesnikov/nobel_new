493220ec4566112b3f1521392e9edb64bc830884
Jurijs Kolesnikovs <jurijs_kolesnikovs@inbox.lv>
2019-01-22 16:51:59 +0200

    Basic server setup done

--- server/server.js
+++ server/server.js
@@ -1,16 +1,29 @@
+/* Importing modules */
 const express = require('express')
 const path = require('path')
-const app = express()
+/* Declaring constants */
 const port = process.env.PORT || 3000
-const publicPath = path.join(__dirname, '/../public')
+const app = express()
 
+/* Using middleware */
+app.use(express.static(path.join(__dirname, 'client/public')))
 
-app.use(express.static(publicPath))
+/* Registering routes */
 
+/* GET Routes */
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
 
-app.listen(port, () => {
-    console.log(`App loaded on port ${port}`)
+app.get('/laureates', (req, res) => {
+    res.send('Laureates page')
 })
+
+app.get('/laureates/:id', (req, res) => {
+    res.send(`Laureate id: ${req.params.id}`)
+})
+
+/* POST Routes */
+
+/* Listen */
+app.listen(port, () => console.log(`App started on port ${port}`))
